eb1bffa071c62ab4846495c25889443ba7b5a82c
Philip Fourie <philip.fourie@gmail.com>
2025-10-25 11:27:46 +1300

    feat: Ctrl+` added for terminal

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.keymap.set("n", "<C-`>", function()
+  -- vim.keymap.set("n", "<space>st", function()
+  vim.cmd.new()
+  vim.cmd.term()
+  -- vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 15)
+
+  -- Check if the current buffer is a terminal before switching modes
+  if vim.bo.buftype == "terminal" then
+    vim.cmd.startinsert()
+  end
+end)
