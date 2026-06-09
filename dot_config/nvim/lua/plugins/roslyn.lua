305b995c4bf571ffcb78d440a53e90ba97ccb1c5
Philip Fourie <philip.fourie@gmail.com>
2026-06-09 21:41:49 +1200

    Track remaining nvim plugin/config files
    - lua/plugins/mason.lua, roslyn.lua, all-themes.lua,
      disable-news-alert.lua, snacks-animated-scrolling-off.lua
    - lua/config/rzls.lua
    - plugin/after/transparency.lua
    - .neoconf.json
    
    Previously orphaned LazyVim-starter files, never chezmoi-added.
    
    (synced from pfl)

---
+++ dot_config/nvim/lua/plugins/roslyn.lua
@@ -0,0 +1,30 @@
+return {
+  "seblyng/roslyn.nvim",
+  ---@module 'roslyn.config'
+  ---@type RoslynNvimConfig
+  -- ft = { "cs", "razor" },
+  opts = {
+    -- your configuration comes here; leave empty for default settings
+  },
+  dependencies = {
+    {
+      -- By loading as a dependencies, we ensure that we are available to set
+      -- the handlers for Roslyn.
+      "tris203/rzls.nvim",
+      config = true,
+    },
+  },
+  -- lazy = false,
+  config = function()
+    require("config.rzls").configure()
+  end,
+  init = function()
+    -- We add the Razor file types before the plugin loads.
+    vim.filetype.add({
+      extension = {
+        razor = "razor",
+        cshtml = "razor",
+      },
+    })
+  end,
+}
