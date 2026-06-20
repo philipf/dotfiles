39d03ed22bb7104afd8734e62e238b3216e0c90f
Philip Fourie <philip.fourie@gmail.com>
2026-06-20 16:46:52 +1200

    Rename snacks-animated-scrolling-off.lua -> snacks-tweaks.lua
    The file had outgrown its name: besides disabling scroll animation it now
    also maximizes the lazygit float (width/height = 0). Rename to reflect that
    it holds misc snacks tweaks, and capture the lazygit styles edit from HOME.
    
    (synced from pfl)

---
+++ dot_config/nvim/lua/plugins/snacks-tweaks.lua
@@ -0,0 +1,14 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		scroll = {
+			enabled = false, -- Disable scrolling animations
+		},
+		styles = {
+			lazygit = {
+				width = 0, -- 0 = full width (maximized)
+				height = 0, -- 0 = full height (maximized)
+			},
+		},
+	},
+}
